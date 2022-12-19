592035fa09000bb973369187643ef1d1e31deba9
Emmanuel Pire <emmanuel@datacamp.com>
2022-12-19 13:21:57 +0100

    remove comments

--- src/detailedCheck.ts
+++ src/detailedCheck.ts
@@ -11,26 +11,6 @@ export async function HealthcheckerDetailedCheck(config: ApplicationConfig): Pro
   const promisesList: Promise<Integration>[] = [];
   const start = new Date().getTime();
   config.integrations.forEach((item) => {
-    // switch (item.type) {
-    //   case HealthTypes.Redis:
-    //     promisesList.push(redisCheck(item));
-    //     break;
-    //   case HealthTypes.Memcached.toString():
-    //     promisesList.push(memcacheCheck(resolveHost(item)));
-    //     break;
-    //   case HealthTypes.Web:
-    //     promisesList.push(webCheck(resolveHost(item)));
-    //     break;
-    //   case HealthTypes.Dynamo:
-    //     promisesList.push(dynamoCheck(resolveHost(item)));
-    //     break;
-    //   case HealthTypes.Database:
-    //     promisesList.push(databaseCheck(resolveHost(item)));
-    //     break;
-    //   case HealthTypes.Custom:
-    //     promisesList.push(customCheck(item));
-    //     break;
-    // }
     promisesList.push(item.check(resolveHost(item)));
   });
   const results = await Promise.all(promisesList);
